9521d7d4f5bc3d03726a775b6a457b047448c42c
nck4s <nn.2nn@mail.ru>
2024-04-01 21:09:06 +1000

    news template

--- news/views.py
+++ news/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from .models import News
 
 
 def news(request):
-    return render (request, 'news.html')
+    newss = News.objects.all().order_by('-date') # most recent news first
+    return render (request, 'news.html', {'newss': newss})
